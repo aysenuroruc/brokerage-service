b745e29e60ff9e869495e7ed0b4b1a28b3b11580
Ayse <ayse@192.168.1.11>
2025-10-26 01:39:32 +0300

    Initial commit

---
+++ src/test/java/com/brokerage/service/BrokerageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokerage.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
